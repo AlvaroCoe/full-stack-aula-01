316e4ab7604475b78671e1afe78b50790e9528f9
niltonsSantana-devBa <nilton.s.santana@ba.estudante.senai.br>
2026-06-16 21:06:13 -0300

    Add Header + Footer

--- projeto-fullstack/src/components/Header/index.jsx
+++ projeto-fullstack/src/components/Header/index.jsx
@@ -1,1 +1,14 @@
 import './style.css'
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <h1>Gerenciamente de Usários</h1>
+            <nav>
+                <Link to="/"> Início</Link>
+                <Link to="/cadastro">Cadastro</Link>
+                <Link to="/usuarios"> Lista de Usuários</Link>
+            </nav>
+        </header>
+    )
+}
