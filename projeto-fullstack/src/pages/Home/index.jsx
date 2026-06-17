14cc5ce7199984d13c599d6dcbea5a884e094af9
AlvaroCoe <alvarodlcoelho@gmail.com>
2026-06-16 21:14:50 -0300

    Inicio Back-end

--- projeto-fullstack/src/pages/Home/index.jsx
+++ projeto-fullstack/src/pages/Home/index.jsx
@@ -1,1 +1,12 @@
 import './style.css'
+
+export default function Home () {
+    return (
+        <div className='home'>
+            <h2> Bem-vindo ao Gerenciamento do Usuário</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, inventore. Dolor cumque, debitis qui optio fugiat quos sequi hic pariatur corporis dicta ea accusamus nisi. Illum impedit aperiam nostrum ea?
+            </p>
+        </div>
+    )
+}
